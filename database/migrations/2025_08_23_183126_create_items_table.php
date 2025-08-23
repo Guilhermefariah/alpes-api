a8cba1a80955129caec2eed22c673bb373c43fd7
Guilhermefariah <Guilhermefaria321@gmail.com>
2025-08-23 18:25:57 -0300

    feat: migration tabela items e  configuração phpMyAdmin no Docker

--- database/migrations/2025_08_23_183126_create_items_table.php
+++ database/migrations/2025_08_23_183126_create_items_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
+            $table->string('code')->unique();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->decimal('price', 10, 2)->nullable();
             $table->timestamps();
         });
     }
